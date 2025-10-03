4db7aac53b7de508245da6ff137ed39099e5007e
kularathnapehesara-commits <kularathnapehesara@gmail.com>
2025-10-03 21:02:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=5cxEiKSb#FyCWbJdbtcLYILWb7sBHJD37BpSuxp6eNRd1c9gNgng' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=QURSVJIK#ehPEynl_GcaEq1hFBoP4Hp75Zj75t8mX_H8-l3tnMnc' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'nadeenmd' : process.env.SESSION_NAME
 };
